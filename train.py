fcbb171b0bcf28de0b36ddcf61de4c346c364e98
Michael Sánchez <kwabena.msm@gmail.com>
2024-07-28 10:37:12 +0200

    Remove max_steps per episode

--- train.py
+++ train.py
@@ -12,8 +12,8 @@ if __name__ == '__main__':
 
     epsilon = 0.2
     gamma = 0.99
-    gae_lambda = 0.95
-    entropy_coef = 0
+    gae_lambda = 0.8
+    entropy_coef = 1e-4
     batch_size = 32
     T = 2048
     actor_lr = 3e-4
@@ -23,8 +23,7 @@ if __name__ == '__main__':
     env = gym.make("BipedalWalker-v3", hardcore=False, render_mode=None)
     trainer = Trainer(epsilon, gamma, gae_lambda, entropy_coef, batch_size, actor_lr, critic_lr, n_epochs)
 
-    n_episodes = 1000
-    max_steps = 600
+    n_episodes = 500
     episode_rewards = []
     avg_rewards = []
     max_reward = -1000
@@ -34,7 +33,7 @@ if __name__ == '__main__':
         terminated = truncated = False
         episode_reward = 0
 
-        for _ in range(max_steps):
+        while not terminated and not truncated:
             action, prob, value = trainer.agent.choose_action(state)
             new_state, reward, terminated, truncated, _ = env.step(action)
             trainer.remember(state, action, reward, value, prob, terminated)
@@ -43,9 +42,6 @@ if __name__ == '__main__':
             if len(trainer.memory.states) >= T:
                 trainer.learn()
                 trainer.memory.clear_memory()
-
-            if terminated or truncated:
-                break
 
             state = new_state
 
